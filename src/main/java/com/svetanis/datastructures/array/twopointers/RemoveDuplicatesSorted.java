f7fa9ca7f47e75259d78c018c3ba61ecd8a3a9e7
sveta <svet125@gmail.com>
2019-08-24 09:40:01 -0700

    [sveta] remove duplicates from sorted array in place

--- src/main/java/com/svetanis/datastructures/array/twopointers/RemoveDuplicatesSorted.java
+++ src/main/java/com/svetanis/datastructures/array/twopointers/RemoveDuplicatesSorted.java
@@ -1,30 +1,43 @@
 package com.svetanis.datastructures.array.twopointers;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.java.base.utils.Print.print;
-import static java.util.Arrays.sort;
-import static org.apache.commons.lang3.ArrayUtils.toObject;
+import static java.util.Collections.sort;
+
+import java.util.List;
+
+// Remove duplicates from Sorted Array
+// Given a sorted array, remove the duplicates in place 
+// such that each element appears only once and return the new length.
 
 public final class RemoveDuplicatesSorted {
 
-  public static int remove(int[] a) {
+  public static int remove(List<Integer> a) {
     // Time Complexity: O(n log n)
     // Space Complexity: O(1)
 
-    int n = a.length;
     sort(a);
     int i = 0;
+    int n = a.size();
     for (int j = 1; j < n; j++) {
-      if (a[j] != a[i]) {
+      if (!a.get(j).equals(a.get(i))) {
         i++;
-        a[i] = a[j];
+        a.set(i, a.get(j));
       }
+    }
+    for (int k = i + 1; k < n; k++) {
+      a.set(k, null);
     }
     return i + 1;
   }
 
   public static void main(String[] args) {
-    int[] a = { 6, 7, 7, 3, 1, 5, 5, 9, 9, 3 };
-    int k = remove(a);
-    print(toObject(a), k - 1);
+    List<Integer> a = newArrayList(6, 7, 7, 3, 1, 5, 5, 9, 9, 3);
+    System.out.println(remove(a));
+    print(a);
+
+    List<Integer> a2 = newArrayList(5000, 5000, 5000);
+    System.out.println(remove(a2));
+    print(a2);
   }
 }
