00f444eb06424e57be5f40847efa427706b64462
sveta <svet125@gmail.com>
2019-09-02 16:40:44 -0700

    [sveta] print cousins

---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/PrintCousinsIterative.java
@@ -0,0 +1,67 @@
+package com.svetanis.datastructures.tree.binary.bt;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.google.common.collect.Lists.newLinkedList;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
+import static com.svetanis.java.base.collect.Lists.newList;
+import static com.svetanis.java.base.utils.Print.print;
+
+import java.util.List;
+import java.util.Queue;
+
+import com.google.common.collect.ImmutableList;
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+public final class PrintCousinsIterative {
+
+  public static ImmutableList<Integer> cousins(Node root, Node node) {
+    // Time complexity: O(n)
+
+    if (root.equals(node)) {
+      return newList();
+    }
+
+    boolean found = false;
+    List<Integer> list = newArrayList();
+    Queue<Node> queue = newLinkedList();
+    queue.offer(root);
+
+    while (!queue.isEmpty() && !found) {
+      int size = queue.size();
+      for (int i = 0; i < size; i++) {
+        Node front = queue.poll();
+        if (node.equals(front.left) || node.equals(front.right)) {
+          found = true;
+        } else {
+          if (isNotNull(front.left)) {
+            queue.offer(front.left);
+          }
+          if (isNotNull(front.right)) {
+            queue.offer(front.right);
+          }
+        }
+      }
+
+      if (found) {
+        for (int i = 0; i < size; i++) {
+          list.add(queue.poll().data);
+        }
+      }
+    }
+    return newList(list);
+  }
+
+  public static void main(String[] args) {
+    Node root = newNode(1);
+    root.left = newNode(2);
+    root.right = newNode(3);
+    root.left.left = newNode(4);
+    root.left.right = newNode(5);
+    root.left.right.right = newNode(15);
+    root.right.left = newNode(6);
+    root.right.right = newNode(7);
+    root.right.left.right = newNode(8);
+    print(cousins(root, root.left.right));
+  }
+}
---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/PrintCousinsRecursive.java
@@ -0,0 +1,62 @@
+package com.svetanis.datastructures.tree.binary.bt;
+
+import static com.google.common.collect.Lists.newArrayList;
+import static com.svetanis.datastructures.tree.binary.bt.Level.level;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
+import static com.svetanis.java.base.collect.Lists.newList;
+import static com.svetanis.java.base.utils.Print.print;
+
+import java.util.List;
+
+import com.google.common.collect.ImmutableList;
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+public final class PrintCousinsRecursive {
+
+  public static ImmutableList<Integer> cousins(Node root, Node node) {
+    // Time complexity: O(n)
+
+    int level = level(root, node);
+    List<Integer> list = newArrayList();
+    givenLevel(root, node, level, list);
+    return newList(list);
+  }
+
+  private static void givenLevel(Node root, Node node, 
+                        int level, List<Integer> list) {
+
+    if (isNull(root) || level < 2) {
+      return;
+    }
+
+    if (level == 2) {
+      if (root.left == node || root.right == node) {
+        return;
+      }
+      if (isNotNull(root.left)) {
+        list.add(root.left.data);
+      }
+      if (isNotNull(root.right)) {
+        list.add(root.right.data);
+      }
+    } else if (level > 2) {
+      givenLevel(root.left, node, level - 1, list);
+      givenLevel(root.right, node, level - 1, list);
+    }
+  }
+
+  public static void main(String[] args) {
+    Node root = newNode(1);
+    root.left = newNode(2);
+    root.right = newNode(3);
+    root.left.left = newNode(4);
+    root.left.right = newNode(5);
+    root.left.right.right = newNode(15);
+    root.right.left = newNode(6);
+    root.right.right = newNode(7);
+    root.right.left.right = newNode(8);
+    print(cousins(root, root.left.right));
+  }
+}
