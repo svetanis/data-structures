337b97013fcdcb8f5b99eebfc9c58f3f52695580
svetanis <svet125@gmail.com>
2024-07-04 14:58:16 -0700

    pair with given sum and max shortest distance from the endpoints

---
+++ src/main/java/com/svetanis/datastructures/array/pairs/PairGivenSumMaxShortestDist.java
@@ -0,0 +1,56 @@
+package com.svetanis.datastructures.array.pairs;
+
+import static com.google.common.collect.Maps.newHashMap;
+import static com.svetanis.java.base.collect.Maps.newMap;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Math.max;
+import static java.lang.Math.min;
+import java.util.Map;
+import com.google.common.collect.ImmutableMap;
+
+// given an array of n integers and an integer k
+// pick two distinct elements whose sum is k and
+// find the max shortest distance of the picked
+// elements from the endpoints
+
+public final class PairGivenSumMaxShortestDist {
+  // Time Complexity: O(n)
+  // Auxiliary Space: O(n)
+	
+  public static int pair(int[] a, int k) {
+    Map<Integer, Integer> map = distMap(a);
+    return dist(a, k, map);
+  }
+
+  private static int dist(int[] a, int k, Map<Integer, Integer> map) {
+    int min = MAX_VALUE;
+    for (int x : a) {
+      int y = k - x;
+      if (x != y && map.containsKey(y)) {
+        int max = max(map.get(x), map.get(y));
+        min = min(min, max);
+      }
+    }
+    return min;
+  }
+
+  private static ImmutableMap<Integer, Integer> distMap(int[] a) {
+    int n = a.length;
+    Map<Integer, Integer> map = newHashMap();
+    for (int i = 0; i < n; i++) {
+      int min = min(1 + i, n - i);
+      if (map.containsKey(a[i])) {
+        int prev = map.get(a[i]);
+        map.put(a[i], min(prev, min));
+      } else {
+        map.put(a[i], min);
+      }
+    }
+    return newMap(map);
+  }
+
+  public static void main(String[] args) {
+    int[] a = { 3, 5, 8, 6, 7 };
+    System.out.println(pair(a, 11));
+  }
+}
