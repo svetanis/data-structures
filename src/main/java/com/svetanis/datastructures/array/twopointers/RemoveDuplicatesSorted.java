c8503df970bdfe381041252a9958a7f79ea9bea7
svetanis <svet125@gmail.com>
2024-07-21 08:26:22 -0700

    remove duplicates in place

--- src/main/java/com/svetanis/datastructures/array/twopointers/RemoveDuplicatesSorted.java
+++ src/main/java/com/svetanis/datastructures/array/twopointers/RemoveDuplicatesSorted.java
@@ -1,10 +1,7 @@
 package com.svetanis.datastructures.array.twopointers;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.java.base.utils.Print.print;
-import static java.util.Collections.sort;
-
-import java.util.List;
+import static java.util.Arrays.sort;
 
 // Remove duplicates from Sorted Array
 // Given a sorted array, remove the duplicates in place 
@@ -12,32 +9,45 @@ import java.util.List;
 
 public final class RemoveDuplicatesSorted {
 
-  public static int remove(List<Integer> a) {
-    // Time Complexity: O(n log n)
-    // Space Complexity: O(1)
+	public static int remove(int[] a) {
+		// Time Complexity: O(n log n)
+		// Space Complexity: O(1)
 
-    sort(a);
-    int i = 0;
-    int n = a.size();
-    for (int j = 1; j < n; j++) {
-      if (!a.get(j).equals(a.get(i))) {
-        i++;
-        a.set(i, a.get(j));
-      }
-    }
-    for (int k = i + 1; k < n; k++) {
-      a.set(k, null);
-    }
-    return i + 1;
-  }
+		sort(a);
+		int next = 0; // index of the next non-duplicate element
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] != a[next]) {
+				next++;
+				a[next] = a[i];
+			}
+		}
+		return next + 1;
+	}
 
-  public static void main(String[] args) {
-    List<Integer> a = newArrayList(6, 7, 7, 3, 1, 5, 5, 9, 9, 3);
-    System.out.println(remove(a));
-    print(a);
+	public static int remove2(int[] a) {
+		// Time Complexity: O(n log n)
+		// Space Complexity: O(1)
 
-    List<Integer> a2 = newArrayList(5000, 5000, 5000);
-    System.out.println(remove(a2));
-    print(a2);
-  }
+		sort(a);
+		int next = 1; // index of the next non-duplicate element
+		for (int i = 1; i < a.length; i++) {
+			if (a[i] != a[next - 1]) {
+				a[next] = a[i];
+				next++;
+			}
+		}
+		return next;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 6, 7, 7, 3, 1, 5, 5, 9, 9, 3 };
+		int last = remove(a);
+		System.out.println(last);
+		print(a, last);
+
+		int[] a2 = { 5000, 5000, 5000 };
+		int last2 = remove(a2);
+		System.out.println(last2);
+		print(a2, last2);
+	}
 }
