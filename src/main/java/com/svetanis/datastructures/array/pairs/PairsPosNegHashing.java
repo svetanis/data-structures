86ca6d07c84f72171310165f9773865dc715ec72
svetanis <svet125@gmail.com>
2024-07-20 17:28:19 -0700

    pairs positive and negative value of a number

--- src/main/java/com/svetanis/datastructures/array/pairs/PairsPosNegHashing.java
+++ src/main/java/com/svetanis/datastructures/array/pairs/PairsPosNegHashing.java
@@ -1,15 +1,16 @@
 package com.svetanis.datastructures.array.pairs;
 
-import static com.google.common.base.Predicates.not;
 import static com.google.common.collect.Lists.newArrayList;
-import static com.svetanis.java.base.collect.Lists.filter;
-import static com.svetanis.java.base.collect.Lists.newList;
-import static com.svetanis.java.base.collect.Sets.newSet;
+import static com.google.common.collect.Lists.transform;
+import static com.google.common.collect.Maps.filterValues;
+import static com.svetanis.java.base.collect.Lists.transform;
+import static com.svetanis.java.base.utils.Maps.freqMap;
+import static java.lang.Math.abs;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
-import com.google.common.base.Predicate;
 import com.google.common.collect.ImmutableList;
 import com.svetanis.java.base.Pair;
 
@@ -22,21 +23,20 @@ public final class PairsPosNegHashing {
 	// Time Complexity: O(n)
 
 	public static ImmutableList<Pair<Integer, Integer>> pairs(List<Integer> list) {
-	  Predicate<Integer> pep = i -> i >= 0;
-	  List<Integer> positive = filter(list, pep);
-	  Set<Integer> negative = newSet(filter(list, not(pep)));
-	  List<Pair<Integer, Integer>> pairs = newArrayList();
-	  for(int pos : positive) {
-		  int neg = -1 * pos;
-		  if(negative.contains(neg)) {
-			  pairs.add(Pair.build(pos, neg));
-		  }
-	  }
-	  return newList(pairs);
+		List<Integer> transformed = transform(list, i -> abs(i));
+		Map<Integer, Integer> map = freqMap(transformed);
+		Map<Integer, Integer> filtered = filterValues(map, v -> v == 2);
+		Set<Integer> set = filtered.keySet();
+		return transform(set, p -> pair(p));
+	}
+
+	private static Pair<Integer, Integer> pair(int num) {
+		int neg = -1 * num;
+		return Pair.build(num, neg);
 	}
 
 	public static void main(String[] args) {
-		List<Integer> list = newArrayList(4, 8, 9, -4, 1, -1, -8, -9);
+		List<Integer> list = newArrayList(4, 8, 9, -4, 1, -1, -8, -9, 5);
 		System.out.println(pairs(list));
 	}
 }
