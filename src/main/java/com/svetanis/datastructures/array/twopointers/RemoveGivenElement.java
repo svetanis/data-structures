a74d4987f2b7e181c8856823dbee37bee47d84a6
svetanis <svet125@gmail.com>
2024-07-21 08:34:45 -0700

    remove given element

--- src/main/java/com/svetanis/datastructures/array/twopointers/RemoveGivenElement.java
+++ src/main/java/com/svetanis/datastructures/array/twopointers/RemoveGivenElement.java
@@ -1,9 +1,6 @@
 package com.svetanis.datastructures.array.twopointers;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.java.base.utils.Print.print;
-
-import java.util.List;
 
 // Given an array and a value, remove all the instances of that value in the array. 
 // Also return the number of elements left in the array after the operation.
@@ -11,26 +8,22 @@ import java.util.List;
 
 public final class RemoveGivenElement {
 
-  public static int remove(List<Integer> a, int target) {
+  public static int remove(int[] a, int target) {
     // Time Complexity: O(n)
     // Space Complexity: O(1)
 
-    int i = 0;
-    int n = a.size();
-    for (int j = 0; j < n; j++) {
-      if (!a.get(j).equals(target)) {
-        a.set(i, a.get(j));
-        i++;
+    int next = 0; // index of the next element which is not target
+    for (int i = 0; i < a.length; i++) {
+      if (a[i] != target) {
+        a[next] = a[i];
+        next++;
       }
     }
-    for (int k = i; k < n; k++) {
-      a.set(k, null);
-    }
-    return i;
+    return next;
   }
 
   public static void main(String[] args) {
-    List<Integer> a = newArrayList(4, 1, 1, 2, 1, 3);
+    int[] a = {4, 1, 1, 2, 1, 3};
     System.out.println(remove(a, 1));
     print(a);
   }
