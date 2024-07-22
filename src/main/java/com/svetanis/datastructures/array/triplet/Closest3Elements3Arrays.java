e995e483d362405dfc364c514d9aa437e56e69e8
svetanis <svet125@gmail.com>
2024-07-21 19:42:02 -0700

    closest 3 elements in 3 arrays

--- src/main/java/com/svetanis/datastructures/array/triplet/Closest3Elements3Arrays.java
+++ src/main/java/com/svetanis/datastructures/array/triplet/Closest3Elements3Arrays.java
@@ -3,65 +3,81 @@ package com.svetanis.datastructures.array.triplet;
 import static java.lang.Integer.MAX_VALUE;
 import static java.lang.Math.max;
 import static java.lang.Math.min;
+import static java.util.Arrays.sort;
 
 import com.svetanis.java.base.utils.Triplet;
 
-// given 3 sorted arrays, find 3 elements such that
-// max(abs(a[i] - b[j]), abs(b[j] - c[k]), abs(c[k] - a[i])) is minimized
+// given three sorted arrays A[], B[] and C[], 
+// find 3 elements i, j and k from A, B and C respectively 
+// such that max(abs(A[i] – B[j]), abs(B[j] – C[k]), abs(C[k] – A[i])) is minimized. 
 
 public final class Closest3Elements3Arrays {
 
-  public static Triplet<Integer, Integer, Integer> triplet(int[] a, int[] b, int[] c) {
-    // Time complexity: O(n + m + l)
+	public static Triplet<Integer, Integer, Integer> triplet(int[] a, int[] b, int[] c) {
+		// Time complexity: O(n + m + l)
 
-    int target = MAX_VALUE;
+		sort(a);
+		sort(b);
+		sort(c);
 
-    int n = a.length;
-    int m = b.length;
-    int l = c.length;
+		int target = MAX_VALUE;
 
-    int first = 0;
-    int second = 0;
-    int third = 0;
+		int n = a.length;
+		int m = b.length;
+		int l = c.length;
 
-    int i = 0;
-    int j = 0;
-    int k = 0;
+		int first = 0;
+		int second = 0;
+		int third = 0;
 
-    while (i < n && j < m && k < l) {
-      int min = min(a[i], min(b[j], c[k]));
-      int max = max(a[i], max(b[j], c[k]));
-      int dif = max - min;
-      // if current diff is less
-      // than min diff so far
-      if (dif < target) {
-        target = dif;
-        first = i;
-        second = j;
-        third = k;
-      }
+		int i = 0;
+		int j = 0;
+		int k = 0;
 
-      if (target == 0) {
-        break;
-      }
-      
-      // increment index of array
-      // with smallest value
-      if (a[i] == min) {
-        i++;
-      } else if (b[j] == min) {
-        j++;
-      } else {
-        k++;
-      }
-    }
-    return Triplet.build(a[first], b[second], c[third]);
-  }
+		while (i < n && j < m && k < l) {
+			int min = min(a[i], min(b[j], c[k]));
+			int max = max(a[i], max(b[j], c[k]));
+			int dif = max - min;
+			// if current diff is less
+			// than min diff so far
+			if (dif < target) {
+				target = dif;
+				first = i;
+				second = j;
+				third = k;
+			}
 
-  public static void main(String[] args) {
-    int[] a = { 1, 4, 10 };
-    int[] b = { 2, 15, 20 };
-    int[] c = { 10, 12 };
-    System.out.println(triplet(a, b, c));
-  }
+			if (target == 0) {
+				break;
+			}
+
+			// increment index of array
+			// with smallest value
+			if (a[i] == min) {
+				i++;
+			} else if (b[j] == min) {
+				j++;
+			} else {
+				k++;
+			}
+		}
+		return Triplet.build(a[first], b[second], c[third]);
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 1, 4, 10 };
+		int[] b = { 2, 15, 20 };
+		int[] c = { 10, 12 };
+		System.out.println(triplet(a, b, c));
+
+		int[] a1 = { 5, 2, 8 };
+		int[] a2 = { 10, 7, 12 };
+		int[] a3 = { 9, 14, 6 };
+		System.out.println(triplet(a1, a2, a3));
+
+		int[] b1 = { 15, 12, 18, 9 };
+		int[] b2 = { 10, 17, 13, 8 };
+		int[] b3 = { 14, 16, 11, 5 };
+		System.out.println(triplet(b1, b2, b3));
+	}
 }
