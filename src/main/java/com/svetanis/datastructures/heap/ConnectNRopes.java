9824aa1c15f5ef90c66a127cba4662fdddb85bd8
svetanis <svet125@gmail.com>
2024-08-22 19:50:35 -0700

    connect n ropes

--- src/main/java/com/svetanis/datastructures/heap/ConnectNRopes.java
+++ src/main/java/com/svetanis/datastructures/heap/ConnectNRopes.java
@@ -3,36 +3,50 @@ package com.svetanis.datastructures.heap;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
+// given n ropes with different lengths,
+// connect them into one big rope with min cost. 
+// the cost of connecting two ropes is
+// equal to the sum of their lengths
+
 public final class ConnectNRopes {
 
-  public static int minCost(int[] a) {
-    int cost = 0;
-    Queue<Integer> queue = build(a);
-    
-    while (queue.size() > 1) {
-      // extract shortest two ropes from priority queue
-      int first = queue.poll();
-      int second = queue.poll();
-      // connect the ropes: update result and
-      // insert the new rope to priority queue
-      cost += (first + second);
-      queue.offer(first + second);
-    }
-    return cost;
-  }
+	public static int minCost(int[] a) {
+		// Time Complexity: O(n log n)
 
-  private static Queue<Integer> build(int[] a) {
-    int n = a.length;
-    Queue<Integer> queue = new PriorityQueue<>();
-    for (int i = 0; i < n; i++) {
-      queue.offer(a[i]);
-    }
-    return queue;
-  }
+		int cost = 0;
+		Queue<Integer> pq = build(a);
 
-  public static void main(String[] args) {
-    int[] a = { 4, 3, 2, 6 };
-    System.out.println(minCost(a));
-  }
+		while (pq.size() > 1) {
+			// extract shortest two ropes from priority queue
+			int first = pq.poll();
+			int second = pq.poll();
+			// connect the ropes: update result and
+			// insert the new rope to priority queue
+			cost += (first + second);
+			pq.add(first + second);
+		}
+		return cost;
+	}
+
+	private static Queue<Integer> build(int[] a) {
+		Queue<Integer> pq = new PriorityQueue<>((x, y) -> x - y);
+		for (int i = 0; i < a.length; i++) {
+			pq.add(a[i]);
+		}
+		return pq;
+	}
+
+	public static void main(String[] args) {
+		int[] a1 = { 4, 3, 2, 6 };
+		System.out.println(minCost(a1));
+
+		int[] a2 = { 1, 3, 11, 5 };
+		System.out.println(minCost(a2));
+
+		int[] a3 = { 3, 4, 5, 6 };
+		System.out.println(minCost(a3));
+
+		int[] a4 = { 1, 3, 11, 5, 2 };
+		System.out.println(minCost(a4));
+	}
 }
-
