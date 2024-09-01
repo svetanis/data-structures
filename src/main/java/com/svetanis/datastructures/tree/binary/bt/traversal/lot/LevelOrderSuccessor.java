3b2522f33bd47bd13dc8bb4a95a1ce35d830b2f5
svetanis <svet125@gmail.com>
2024-08-31 18:21:27 -0700

    level order traversal coding pattern

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/lot/LevelOrderSuccessor.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/lot/LevelOrderSuccessor.java
@@ -8,36 +8,46 @@ import java.util.Queue;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
+// given a binary tree and a node, 
+// find the level order successor
+// of the given node in the tree
+
+// the level order successor is 
+// the node that appears right 
+// after the given node in the
+// level order traversal
+
 public final class LevelOrderSuccessor {
 
-  public static Node lot(Node root, int key) {
-    // time complexity: O(n)
+	public static Node lot(Node root, int key) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(n)
 
-    Queue<Node> queue = newLinkedList();
-    queue.offer(root);
-    while (!queue.isEmpty()) {
-      Node node = queue.poll();
-      if (isNotNull(node.left)) {
-        queue.offer(node.left);
-      }
-      if (isNotNull(node.right)) {
-        queue.offer(node.right);
-      }
-      if (node.data == key) {
-        break;
-      }
-    }
-    return queue.peek();
-  }
+		Queue<Node> queue = newLinkedList();
+		queue.offer(root);
+		while (!queue.isEmpty()) {
+			Node node = queue.poll();
+			if (isNotNull(node.left)) {
+				queue.offer(node.left);
+			}
+			if (isNotNull(node.right)) {
+				queue.offer(node.right);
+			}
+			if (node.data == key) {
+				break;
+			}
+		}
+		return queue.peek();
+	}
 
-  public static void main(String[] args) {
-    Node root = newNode(1);
-    root.left = newNode(2);
-    root.right = newNode(3);
-    root.left.left = newNode(4);
-    root.left.right = newNode(5);
-    root.right.right = newNode(6);
+	public static void main(String[] args) {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.right = newNode(6);
 
-    System.out.println(lot(root, 4));
-  }
+		System.out.println(lot(root, 4));
+	}
 }
