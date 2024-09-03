16857a7175871931e55be4d44be268f792da68fd
svetanis <svet125@gmail.com>
2024-09-03 10:45:45 -0700

    count all path sum

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/path/CountAllPathSum.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/path/CountAllPathSum.java
@@ -1,9 +1,9 @@
 package com.svetanis.datastructures.tree.binary.bt.path;
 
-import static com.google.common.collect.Lists.newLinkedList;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
 
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ListIterator;
 
@@ -17,43 +17,49 @@ import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
 public final class CountAllPathSum {
 
-  public static int pathCount(Node root, int k) {
-    // Time complexity: O(n)
-    List<Integer> list = newLinkedList();
-    return pathCount(root, k, list);
-  }
+	public static int pathCount(Node root, int k) {
+		// Worst Case Time Complexity (skewed tree) : O(n^2)
+		// Best Case Time Complexity (balanced tree): O(n log n)
 
-  private static int pathCount(Node root, int k, List<Integer> list) {
+		List<Integer> list = new LinkedList<>();
+		return pathCount(root, k, list);
+	}
 
-    if (isNull(root)) {
-      return 0;
-    }
+	private static int pathCount(Node node, int k, List<Integer> path) {
+		if (isNull(node)) {
+			return 0;
+		}
+		int sum = 0;
+		int count = 0;
+		// add current node to the path
+		path.add(node.data);
+		// find the sums of all sub-paths in the current path
+		ListIterator<Integer> iter = path.listIterator(path.size());
+		while (iter.hasPrevious()) {
+			sum += iter.previous();
+			// if the sum of any sub-path equals
+			// target increment the path count
+			if (sum == k) {
+				count++;
+			}
+		}
+		// traverse the left subtree
+		count += pathCount(node.left, k, path);
+		// traverse the right subtree
+		count += pathCount(node.right, k, path);
+		// backtrack: remove the current node from the path
+		// while going up the recursive call stack
+		path.remove(path.size() - 1);
+		return count;
+	}
 
-    int sum = 0;
-    int count = 0;
-    list.add(root.data);
-    ListIterator<Integer> iter = list.listIterator();
-    while (iter.hasPrevious()) {
-      sum += iter.previous();
-      if (sum == k) {
-        count++;
-      }
-    }
-
-    count += pathCount(root.left, k, list);
-    count += pathCount(root.right, k, list);
-
-    list.remove(list.size() - 1);
-    return count;
-  }
-
-  public static void main(String[] args) {
-    Node root = newNode(12);
-    root.left = newNode(7);
-    root.right = newNode(1);
-    root.left.left = newNode(4);
-    root.right.left = newNode(10);
-    root.right.right = newNode(5);
-    System.out.println(pathCount(root, 11));
-  }
+	public static void main(String[] args) {
+		Node root = newNode(12);
+		root.left = newNode(7);
+		root.right = newNode(1);
+		root.left.left = newNode(4);
+		root.right.left = newNode(10);
+		root.right.right = newNode(5);
+		System.out.println(pathCount(root, 11));
+	}
 }
