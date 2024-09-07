76dc30eca32834811f7e790f7309312382cef6a5
svetanis <svet125@gmail.com>
2024-09-07 15:50:06 -0700

    closest to k element in bst

---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bst/ClosestToKElementIterative.java
@@ -0,0 +1,59 @@
+package com.svetanis.datastructures.tree.binary.bst;
+
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Math.abs;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// given a BST and a target node K
+// find the node with minimum absolute 
+// difference with given target K
+
+public final class ClosestToKElementIterative {
+
+	public static int closestToK(Node root, int k) {
+		// Time complexity: O(h)
+
+		if (isNull(root)) {
+			return -1;
+		}
+		if (root.data == k) {
+			return root.data;
+		}
+
+		int key = -1;
+		int min = MAX_VALUE;
+		while (isNotNull(root)) {
+			int diff = abs(k - root.data);
+			if (min > diff) {
+				min = diff;
+				key = root.data;
+			}
+			if (k < root.data) {
+				root = root.left;
+			} else {
+				root = root.right;
+			}
+		}
+		return key;
+	}
+
+	public static void main(String[] args) {
+		Node root = newNode(9);
+		root.left = newNode(4);
+		root.right = newNode(17);
+		root.left.left = newNode(3);
+		root.left.right = newNode(6);
+		root.left.right.left = newNode(5);
+		root.left.right.right = newNode(7);
+		root.right.right = newNode(22);
+		root.right.right.left = newNode(20);
+		System.out.println(closestToK(root, 4)); // 4
+		System.out.println(closestToK(root, 18)); // 17
+		System.out.println(closestToK(root, 12)); // 9
+		System.out.println(closestToK(root, 2)); // 3
+	}
+}
---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bst/ClosestToKElementRecursive.java
@@ -0,0 +1,63 @@
+package com.svetanis.datastructures.tree.binary.bst;
+
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
+import static java.lang.Integer.MAX_VALUE;
+import static java.lang.Math.abs;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// given a BST and a target node K
+// find the node with minimum absolute 
+// difference with given target K
+
+public final class ClosestToKElementRecursive {
+
+	public static int closestToK(Node root, int k) {
+		if (root.data == k) {
+			return root.data;
+		}
+		AtomicInteger min = new AtomicInteger(MAX_VALUE);
+		AtomicInteger key = new AtomicInteger(-1);
+		closestToK(root, k, min, key);
+		return key.get();
+	}
+
+	public static void closestToK(Node root, int k, AtomicInteger min, AtomicInteger key) {
+		// Time complexity: O(h)
+
+		if (isNull(root)) {
+			return;
+		}
+
+		int diff = abs(k - root.data);
+		if (min.get() > diff) {
+			min.set(diff);
+			key.set(root.data);
+		}
+
+		if (k < root.data) {
+			closestToK(root.left, k, min, key);
+		} else {
+			closestToK(root.right, k, min, key);
+		}
+	}
+
+	public static void main(String[] args) {
+		Node root = newNode(9);
+		root.left = newNode(4);
+		root.right = newNode(17);
+		root.left.left = newNode(3);
+		root.left.right = newNode(6);
+		root.left.right.left = newNode(5);
+		root.left.right.right = newNode(7);
+		root.right.right = newNode(22);
+		root.right.right.left = newNode(20);
+		System.out.println(closestToK(root, 4)); // 4
+		System.out.println(closestToK(root, 18)); // 17
+		System.out.println(closestToK(root, 12)); // 9
+		System.out.println(closestToK(root, 2)); // 3
+	}
+}
