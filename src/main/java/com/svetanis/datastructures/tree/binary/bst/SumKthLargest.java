760e95f783fdf3c786e6770424735f6c1c78a818
svetanis <svet125@gmail.com>
2024-09-07 12:35:21 -0700

    sum k smallest and sum k largest in bst

--- src/main/java/com/svetanis/datastructures/tree/binary/bst/SumKthLargest.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bst/SumKthLargest.java
@@ -2,57 +2,61 @@ package com.svetanis.datastructures.tree.binary.bst;
 
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
-import static com.svetanis.java.base.utils.IntWrapper.newIntWrapper;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
-import com.svetanis.java.base.utils.IntWrapper;
+
+// given a BST, find the sum of all
+// elements greater than and equal
+// to k-th largest element
 
 public final class SumKthLargest {
 
-  public static int sum(Node root, int k) {
-    // Time complexity: O(k)
+	public static int sum(Node root, int k) {
+		// Time complexity: O(k)
 
-    // count of nodes visited
-    IntWrapper count = newIntWrapper();
-    return sum(root, k, count);
-  }
+		// count of nodes visited
+		AtomicInteger count = new AtomicInteger();
+		return sum(root, k, count);
+	}
 
-  private static int sum(Node root, int k, IntWrapper count) {
+	private static int sum(Node root, int k, AtomicInteger count) {
 
-    if (isNull(root) || count.value > k) {
-      return 0;
-    }
+		if (isNull(root) || count.get() > k) {
+			return 0;
+		}
 
-    // follow inorder traversal
-    // recur for left subtree first
-    int sum = sum(root.right, k, count);
+		// follow reverse in-order traversal
+		// recur for right subtree first
+		int sum = sum(root.right, k, count);
 
-    if (count.value >= k) {
-      return sum;
-    }
+		if (count.get() >= k) {
+			return sum;
+		}
 
-    // add root's data
-    sum += root.data;
+		// add root's data
+		sum += root.data;
 
-    // increment count of visited nodes
-    count.value++;
+		// increment count of visited nodes
+		int curr = count.incrementAndGet();
 
-    if (count.value >= k) {
-      return sum;
-    }
+		if (curr >= k) {
+			return sum;
+		}
 
-    // if count < k, recur for right subtree
-    return sum + sum(root.left, k, count);
-  }
+		// if count < k, recur for left subtree
+		return sum + sum(root.left, k, count);
+	}
 
-  public static void main(String[] args) {
-    Node root = newNode(19);
-    root.left = newNode(7);
-    root.right = newNode(21);
-    root.left.left = newNode(3);
-    root.left.right = newNode(11);
-    root.left.right.left = newNode(9);
-    root.left.right.right = newNode(13);
-    System.out.println(sum(root, 2));
-  }
+	public static void main(String[] args) {
+		Node root = newNode(19);
+		root.left = newNode(7);
+		root.right = newNode(21);
+		root.left.left = newNode(3);
+		root.left.right = newNode(11);
+		root.left.right.left = newNode(9);
+		root.left.right.right = newNode(13);
+		System.out.println(sum(root, 2));
+	}
 }
--- src/main/java/com/svetanis/datastructures/tree/binary/bst/SumKthSmallest.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bst/SumKthSmallest.java
@@ -2,57 +2,68 @@ package com.svetanis.datastructures.tree.binary.bst;
 
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
-import static com.svetanis.java.base.utils.IntWrapper.newIntWrapper;
+
+import java.util.concurrent.atomic.AtomicInteger;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
-import com.svetanis.java.base.utils.IntWrapper;
+
+// given a BST and an integer k
+// find the sum of all elements
+// smaller than or equal to Kth
+// smallest element
+
+// in-order traversal of BST
+// sorts elements in ascending order
+// while traversing, count nodes
+// visited so far and keep adding
+// nodes until count becomes k
 
 public final class SumKthSmallest {
 
-  public static int sum(Node root, int k) {
-    // Time complexity: O(k)
+	public static int sum(Node root, int k) {
+		// Time complexity: O(h)
 
-    // count of nodes visited
-    IntWrapper count = newIntWrapper();
-    return sum(root, k, count);
-  }
+		// count of nodes visited
+		AtomicInteger count = new AtomicInteger();
+		return sum(root, k, count);
+	}
 
-  private static int sum(Node root, int k, IntWrapper count) {
+	private static int sum(Node root, int k, AtomicInteger count) {
 
-    if (isNull(root) || count.value > k) {
-      return 0;
-    }
+		if (isNull(root) || count.get() > k) {
+			return 0;
+		}
 
-    // follow inorder traversal
-    // recur for left subtree first
-    int sum = sum(root.left, k, count);
+		// follow inorder traversal
+		// recur for left subtree first
+		int sum = sum(root.left, k, count);
 
-    if (count.value >= k) {
-      return sum;
-    }
+		if (count.get() >= k) {
+			return sum;
+		}
 
-    // add root's data
-    sum += root.data;
+		// add root's data
+		sum += root.data;
 
-    // increment count of visited nodes
-    count.value++;
+		// increment count of visited nodes
+		int curr = count.incrementAndGet();
 
-    if (count.value >= k) {
-      return sum;
-    }
+		if (curr >= k) {
+			return sum;
+		}
 
-    // if count < k, recur for right subtree
-    return sum + sum(root.right, k, count);
-  }
+		// if count < k, recur for right subtree
+		return sum + sum(root.right, k, count);
+	}
 
-  public static void main(String[] args) {
-    Node root = newNode(20);
-    root.left = newNode(8);
-    root.right = newNode(22);
-    root.left.left = newNode(4);
-    root.left.right = newNode(12);
-    root.left.right.left = newNode(10);
-    root.left.right.right = newNode(22);
-    System.out.println(sum(root, 3));
-  }
+	public static void main(String[] args) {
+		Node root = newNode(20);
+		root.left = newNode(8);
+		root.right = newNode(22);
+		root.left.left = newNode(4);
+		root.left.right = newNode(12);
+		root.left.right.left = newNode(10);
+		root.left.right.right = newNode(22);
+		System.out.println(sum(root, 3));
+	}
 }
