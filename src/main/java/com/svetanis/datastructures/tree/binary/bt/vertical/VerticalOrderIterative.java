613d2371e8f65ca74b5f8b43bce2b7b3f64719ee
svetanis <svet125@gmail.com>
2024-09-29 09:59:03 -0700

    vertical order traversal

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/vertical/VerticalOrderIterative.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/vertical/VerticalOrderIterative.java
@@ -14,52 +14,76 @@ import com.google.common.collect.Multimap;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 import com.svetanis.java.base.Pair;
 
+// given a binary tree, perform vertical 
+// traversal of it: print nodes of a binary tree
+// in vertical order by assuming that the left and
+// right child of a node makes 45 degree angle with 
+// the parent
+
 public final class VerticalOrderIterative {
+	// Time Complexity: O(n)
+	// Aux Space: O(n)
 
-  public static ImmutableMultimap<Integer, Integer> verticalOrder(Node root) {
+	public static ImmutableMultimap<Integer, Integer> verticalOrder(Node root) {
+		Multimap<Integer, Integer> mm = LinkedHashMultimap.create();
+		Queue<Pair<Node, Integer>> queue = newLinkedList();
+		queue.offer(Pair.build(root, 0));
+		while (!queue.isEmpty()) {
+			Pair<Node, Integer> pair = queue.poll();
+			int dist = pair.getRight();
+			Node node = pair.getLeft();
+			mm.put(dist, node.data);
+			if (isNotNull(node.left)) {
+				queue.offer(Pair.build(node.left, dist - 1));
+			}
+			if (isNotNull(node.right)) {
+				queue.offer(Pair.build(node.right, dist + 1));
+			}
+		}
+		return newMultimap(mm);
+	}
 
-    // Time Complexity: O(n)
-    // Aux Space: O(n)
+	public static void main(String[] args) {
+		Node root1 = tree1();
+		print(verticalOrder(root1));
+		System.out.println();
+		Node root2 = tree2();
+		print(verticalOrder(root2));
+	}
 
-    Multimap<Integer, Integer> mm = LinkedHashMultimap.create();
-    Queue<Pair<Node, Integer>> queue = newLinkedList();
-    queue.offer(Pair.build(root, 0));
-    while (!queue.isEmpty()) {
-      Pair<Node, Integer> pair = queue.poll();
-      int dist = pair.getRight();
-      Node node = pair.getLeft();
-      mm.put(dist, node.data);
-      if (isNotNull(node.left)) {
-        queue.offer(Pair.build(node.left, dist - 1));
-      }
-      if (isNotNull(node.right)) {
-        queue.offer(Pair.build(node.right, dist + 1));
-      }
-    }
-    return newMultimap(mm);
-  }
+	private static Node tree2() {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.right.left = newNode(5);
+		root.right.right = newNode(6);
+		root.right.left.left = newNode(7);
+		root.right.left.right = newNode(8);
+		root.right.left.right.left = newNode(9);
+		root.right.left.right.right = newNode(10);
+		return root;
+	}
 
-  public static void main(String[] args) {
+	private static Node tree1() {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.left = newNode(6);
+		root.right.right = newNode(7);
+		root.right.left.right = newNode(8);
+		root.right.right.right = newNode(9);
+		root.right.right.left = newNode(10);
+		root.right.right.left.right = newNode(11);
+		root.right.right.left.right.right = newNode(12);
+		return root;
+	}
 
-    Node root = newNode(1);
-    root.left = newNode(2);
-    root.right = newNode(3);
-    root.left.left = newNode(4);
-    root.left.right = newNode(5);
-    root.right.left = newNode(6);
-    root.right.right = newNode(7);
-    root.right.left.right = newNode(8);
-    root.right.right.right = newNode(9);
-    root.right.right.left = newNode(10);
-    root.right.right.left.right = newNode(11);
-    root.right.right.left.right.right = newNode(12);
-    print(verticalOrder(root));
-  }
-
-  // -2: [4]
-  // -1: [2]
-  //  0: [1, 5, 6]
-  //  1: [3, 8, 10]
-  //  2: [7, 11]
-  //  3: [9, 12]
+	// -2: [4]
+	// -1: [2]
+	// 0: [1, 5, 6]
+	// 1: [3, 8, 10]
+	// 2: [7, 11]
+	// 3: [9, 12]
 }
--- src/main/java/com/svetanis/datastructures/tree/binary/bt/vertical/VerticalOrderRecursive.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/vertical/VerticalOrderRecursive.java
@@ -10,45 +10,50 @@ import com.google.common.collect.LinkedHashMultimap;
 import com.google.common.collect.Multimap;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
+// given a binary tree, perform vertical 
+// traversal of it: print nodes of a binary tree
+// in vertical order by assuming that the left and
+// right child of a node makes 45 degree angle with 
+// the parent
+
 public final class VerticalOrderRecursive {
+	// Time Complexity: O(n)
 
-  public static ImmutableMultimap<Integer, Integer> verticalOrder(Node root) {
-    // Time Complexity: O(n)
+	public static ImmutableMultimap<Integer, Integer> verticalOrder(Node root) {
+		Multimap<Integer, Integer> mm = LinkedHashMultimap.create();
+		verticalOrder(root, 0, mm);
+		return newMultimap(mm);
+	}
 
-    Multimap<Integer, Integer> mm = LinkedHashMultimap.create();
-    verticalOrder(root, 0, mm);
-    return newMultimap(mm);
-  }
+	private static void verticalOrder(Node node, int dist, Multimap<Integer, Integer> mm) {
+		if (isNull(node)) {
+			return;
+		}
+		mm.put(dist, node.data);
+		verticalOrder(node.left, dist - 1, mm);
+		verticalOrder(node.right, dist + 1, mm);
+	}
 
-  private static void verticalOrder(Node node, int dist, Multimap<Integer, Integer> mm) {
-    if (isNull(node)) {
-      return;
-    }
-    mm.put(dist, node.data);
-    verticalOrder(node.left, dist - 1, mm);
-    verticalOrder(node.right, dist + 1, mm);
-  }
+	public static void main(String[] args) {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.left = newNode(6);
+		root.right.right = newNode(7);
+		root.right.left.right = newNode(8);
+		root.right.right.right = newNode(9);
+		root.right.right.left = newNode(10);
+		root.right.right.left.right = newNode(11);
+		root.right.right.left.right.right = newNode(12);
+		print(verticalOrder(root));
+	}
 
-  public static void main(String[] args) {
-    Node root = newNode(1);
-    root.left = newNode(2);
-    root.right = newNode(3);
-    root.left.left = newNode(4);
-    root.left.right = newNode(5);
-    root.right.left = newNode(6);
-    root.right.right = newNode(7);
-    root.right.left.right = newNode(8);
-    root.right.right.right = newNode(9);
-    root.right.right.left = newNode(10);
-    root.right.right.left.right = newNode(11);
-    root.right.right.left.right.right = newNode(12);
-    print(verticalOrder(root));
-  }
-  
-      // -2: [4]
-      // -1: [2]
-      //  0: [1, 5, 6]
-      //  1: [3, 8, 10]
-      //  2: [7, 11]
-      //  3: [9, 12]
+	// -2: [4]
+	// -1: [2]
+	// 0: [1, 5, 6]
+	// 1: [3, 8, 10]
+	// 2: [7, 11]
+	// 3: [9, 12]
 }
