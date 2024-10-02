9cdb73ec297ca81e44733f018a4392d8306b4eef
svetanis <svet125@gmail.com>
2024-10-01 18:35:04 -0700

    majority element

--- src/main/java/com/svetanis/datastructures/array/majority/MajorityElementMooreVotingAlgo.java
+++ src/main/java/com/svetanis/datastructures/array/majority/MajorityElementMooreVotingAlgo.java
@@ -2,51 +2,52 @@ package com.svetanis.datastructures.array.majority;
 
 import static com.google.common.base.Optional.absent;
 import static com.google.common.base.Optional.of;
-import static com.svetanis.datastructures.array.majority.MajorityElementGuava.isMajority;
 
 import com.google.common.base.Optional;
 
-// A majority element in an array A[] of size n is an element that appears more than n/2 times 
+// A majority element in an array a[] of size n 
+// is an element that appears more than n/2 times 
 
 public final class MajorityElementMooreVotingAlgo {
 
-  public static Optional<Integer> majorityElement(int[] a) {
-    // Time Complexity: O(n)
-    // Space Complexity: O(1)
+	public static Optional<Integer> majorityElement(int[] a) {
+		// Time Complexity: O(n)
+		// Space Complexity: O(1)
 
-    // 1. find candidate for majority
-    int candidate = findCandidate(a);
+		// 1. find candidate for majority
+		int index = findCandidate(a);
 
-    // 2. check if candidate is majority
-    if (isMajority(a, candidate)) {
-      return of(candidate);
-    }
+		// 2. check if candidate is majority
+		if (isMajority(a, index)) {
+			return of(a[index]);
+		}
+		return absent();
+	}
 
-    return absent();
-  }
+	private static boolean isMajority(int[] a, int index) {
+		int n = a.length;
+		return index + n / 2 < n && a[index + n / 2] == a[index];
+	}
 
-  private static int findCandidate(int[] a) {
-    int index = 0;
-    int count = 1;
-    int n = a.length;
+	private static int findCandidate(int[] a) {
+		int index = 0;
+		int count = 1;
+		for (int i = 1; i < a.length; ++i) {
+			if (a[i] == a[index]) {
+				++count;
+			} else {
+				--count;
+			}
+			if (count == 0) {
+				index = i;
+				count = 1;
+			}
+		}
+		return index;
+	}
 
-    for (int i = 1; i < n; ++i) {
-      if (a[i] == a[index]) {
-        ++count;
-      } else {
-        --count;
-      }
-
-      if (count == 0) {
-        index = i;
-        count = 1;
-      }
-    }
-    return a[index];
-  }
-
-  public static void main(String[] args) {
-    int[] a = { 1, 3, 3, 3, 3, 1, 2 };
-    System.out.println(majorityElement(a));
-  }
+	public static void main(String[] args) {
+		int[] a = { 1, 3, 3, 3, 3, 1, 2 };
+		System.out.println(majorityElement(a));
+	}
 }
