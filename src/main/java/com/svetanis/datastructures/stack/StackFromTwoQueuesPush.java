f795711fa4dc6bb7afb0757664203a8bb624f9cc
svetanis <svet125@gmail.com>
2024-10-04 19:58:15 -0700

    stack from two queues

--- src/main/java/com/svetanis/datastructures/stack/StackFromTwoQueuesPush.java
+++ src/main/java/com/svetanis/datastructures/stack/StackFromTwoQueuesPush.java
@@ -1,62 +1,66 @@
 package com.svetanis.datastructures.stack;
 
-import static com.google.common.collect.Lists.newLinkedList;
 import static com.svetanis.java.base.Exceptions.illegalState;
 
-import java.util.Queue;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public final class StackFromTwoQueuesPush {
 
-  private Queue<Integer> q1;
-  private Queue<Integer> q2;
+	private Deque<Integer> main;
+	private Deque<Integer> aux;
 
-  public StackFromTwoQueuesPush() {
-    this.q1 = newLinkedList();
-    this.q2 = newLinkedList();
-  }
+	public StackFromTwoQueuesPush() {
+		this.main = new ArrayDeque<>();
+		this.aux = new ArrayDeque<>();
+	}
 
-  public boolean isEmpty() {
-    return q1.isEmpty();
-  }
+	public boolean isEmpty() {
+		return main.isEmpty();
+	}
 
-  public void push(int value) {
-    q2.offer(value);
-    while (!q1.isEmpty()) {
-      q2.offer(q1.poll());
-    }
-    // swap q1 and q2
-    Queue<Integer> temp = q1;
-    q1 = q2;
-    q2 = temp;
-  }
+	public void push(int value) {
+		// add element to aux queue
+		aux.offer(value);
+		// move all elements from
+		// main queue to aux queue
+		// reversing the order
+		while (!main.isEmpty()) {
+			aux.offer(main.poll());
+		}
+		// swap the main and aux queues
+		Deque<Integer> temp = main;
+		main = aux;
+		aux = temp;
+	}
 
-  public int pop() {
-    if (q1.isEmpty()) {
-      throw illegalState("stack underflow");
-    } else {
-      return q1.poll();
-    }
-  }
+	public int pop() {
+		if (main.isEmpty()) {
+			throw illegalState("stack underflow");
+		} else {
+			return main.poll();
+		}
+	}
 
-  public int top() {
-    if (q1.isEmpty()) {
-      throw illegalState("stack underflow");
-    } else {
-      return q1.peek();
-    }
-  }
+	public int top() {
+		if (main.isEmpty()) {
+			throw illegalState("stack underflow");
+		} else {
+			return main.peek();
+		}
+	}
 
-  public static void main(String[] args) {
-    StackFromTwoQueuesPush stack = new StackFromTwoQueuesPush();
-    stack.push(24);
-    stack.push(34);
-    stack.push(4);
-    stack.push(10);
-    stack.push(1);
-    stack.push(43);
-    stack.push(21);
-    System.out.println(stack.pop());
-    System.out.println(stack.pop());
-    System.out.println(stack.pop());
-  }
+	public static void main(String[] args) {
+		StackFromTwoQueuesPush stack = new StackFromTwoQueuesPush();
+		stack.push(24);
+		stack.push(34);
+		stack.push(4);
+		stack.push(10);
+		stack.push(1);
+		stack.push(43);
+		stack.push(21);
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+		System.out.println(stack.pop());
+	}
 }
