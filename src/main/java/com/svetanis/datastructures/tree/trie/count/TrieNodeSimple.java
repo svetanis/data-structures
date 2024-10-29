836f683829a8d7a633052d1b30d6ecc14934303d
svetanis <svet125@gmail.com>
2024-10-29 14:22:29 -0700

    trie node simple

---
+++ src/main/java/com/svetanis/datastructures/tree/trie/count/TrieNodeSimple.java
@@ -0,0 +1,35 @@
+package com.svetanis.datastructures.tree.trie.count;
+
+import static com.google.common.collect.Maps.newHashMap;
+
+import java.util.Map;
+
+public final class TrieNodeSimple {
+
+	protected int freq;
+	protected Map<Character, TrieNodeSimple> children;
+
+	public TrieNodeSimple() {
+		this.freq = 0;
+		this.children = newHashMap();
+	}
+
+	public void insert(String s) {
+		TrieNodeSimple node = this;
+		for (char c : s.toCharArray()) {
+			node.children.putIfAbsent(c, new TrieNodeSimple());
+			node = node.children.get(c);
+			node.freq++;
+		}
+	}
+
+	public int query(String prefix) {
+		TrieNodeSimple node = this;
+		for (char c : prefix.toCharArray()) {
+			if (!node.children.containsKey(c)) {
+				return 0;
+			}
+		}
+		return node.freq;
+	}
+}
