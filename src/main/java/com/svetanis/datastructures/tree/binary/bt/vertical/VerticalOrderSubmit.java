0d39404c67e39a9258ec939f2e7d31e275bbae59
svetanis <svet125@gmail.com>
2024-10-30 09:18:40 -0700

    bt: vertical order

---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/vertical/VerticalOrderSubmit.java
@@ -0,0 +1,105 @@
+package com.svetanis.datastructures.tree.binary.bt.vertical;
+
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+import static com.svetanis.java.base.utils.Print.print;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.TreeMap;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// 314. Binary Tree Vertical Order Traversal
+
+// given a binary tree, perform vertical 
+// traversal of it: print nodes of a binary tree
+// in vertical order by assuming that the left and
+// right child of a node makes 45 degree angle with 
+// the parent
+
+public final class VerticalOrderSubmit {
+	// Time Complexity: O(n)
+	// Aux Space: O(n)
+
+	public static List<List<Integer>> verticalOrder(Node root) {
+		if (root == null) {
+			return new ArrayList<>();
+		}
+		Map<Integer, List<Integer>> map = new TreeMap<>();
+		Queue<Pair> queue = new ArrayDeque<>();
+		queue.offer(new Pair(root, 0));
+		while (!queue.isEmpty()) {
+			Pair pair = queue.poll();
+			int dist = pair.dist;
+			Node node = pair.node;
+			List<Integer> list = map.getOrDefault(dist, new ArrayList<>());
+			list.add(node.data);
+			map.put(dist, list);
+			// in a single line:
+			// map.computeIfAbsent(dist, k -> new ArrayList<>()).add(node.data);
+			if (node.left != null) {
+				queue.offer(new Pair(node.left, dist - 1));
+			}
+			if (node.right != null) {
+				queue.offer(new Pair(node.right, dist + 1));
+			}
+		}
+		List<List<Integer>> lists = new ArrayList<>();
+		lists.addAll(map.values());
+		return lists;
+	}
+
+	public static void main(String[] args) {
+		Node root1 = tree1();
+		print(verticalOrder(root1));
+		// [4] [2] [1, 5, 6] [3, 8, 10] [7, 11] [9, 12]
+		System.out.println();
+		Node root2 = tree2();
+		print(verticalOrder(root2));
+		// [2, 7] [1, 5, 9] [3, 8] [6, 10]
+	}
+
+	private static Node tree2() {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.right.left = newNode(5);
+		root.right.right = newNode(6);
+		root.right.left.left = newNode(7);
+		root.right.left.right = newNode(8);
+		root.right.left.right.left = newNode(9);
+		root.right.left.right.right = newNode(10);
+		return root;
+	}
+
+	private static Node tree1() {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.left = newNode(6);
+		root.right.right = newNode(7);
+		root.right.left.right = newNode(8);
+		root.right.right.right = newNode(9);
+		root.right.right.left = newNode(10);
+		root.right.right.left.right = newNode(11);
+		root.right.right.left.right.right = newNode(12);
+		return root;
+	}
+
+	private static class Pair {
+
+		private Node node;
+		private int dist;
+
+		public Pair(Node node, int dist) {
+			this.node = node;
+			this.dist = dist;
+		}
+	}
+
+}
