6cc7ac51fc5f8fb75d31f53ec53fed0733fd76ce
svetanis <svet125@gmail.com>
2024-11-10 14:05:45 -0700

    house thief

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/HouseThief.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/HouseThief.java
@@ -5,57 +5,59 @@ import static java.lang.Math.max;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
+// 337. House Robber III
+
 // The houses form a binary tree. 
 // If the root is robbed, 
 // its left and right can not be robbed. 
 
 public final class HouseThief {
 
-  public static int maxProfit(Node root) {
-    if (root == null) {
-      return 0;
-    }
-    Result p = profit(root);
-    return max(p.incl, p.excl);
-  }
+	public static int maxProfit(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		Profit p = profit(root);
+		return max(p.incl, p.excl);
+	}
 
-  private static Result profit(Node root) {
-    if (root == null) {
-      return new Result(0, 0);
-    }
-    Result left = profit(root.left);
-    Result right = profit(root.right);
-    int incl = root.data + left.excl + right.excl;
-    int excl = max(left.excl, left.incl) + max(right.excl, right.incl);
-    return new Result(incl, excl);
-  }
+	private static Profit profit(Node root) {
+		if (root == null) {
+			return new Profit(0, 0);
+		}
+		Profit left = profit(root.left);
+		Profit right = profit(root.right);
+		int incl = root.data + left.excl + right.excl;
+		int excl = max(left.excl, left.incl) + max(right.excl, right.incl);
+		return new Profit(incl, excl);
+	}
 
-  public static void main(String[] args) {
-    Node root1 = newNode(3);
-    root1.left = newNode(2);
-    root1.right = newNode(3);
-    root1.left.right = newNode(3);
-    root1.right.right = newNode(1);
-    System.out.println(maxProfit(root1));
-    
-    Node root2 = newNode(3);
-    root2.left = newNode(4);
-    root2.right = newNode(5);
-    root2.left.left = newNode(1);
-    root2.left.right = newNode(3);
-    root2.right = newNode(5);
-    root2.right.right = newNode(1);
-    System.out.println(maxProfit(root2));
-    
-  }
+	public static void main(String[] args) {
+		Node root1 = newNode(3);
+		root1.left = newNode(2);
+		root1.right = newNode(3);
+		root1.left.right = newNode(3);
+		root1.right.right = newNode(1);
+		System.out.println(maxProfit(root1));
 
-  private static class Result {
-    int incl;
-    int excl;
+		Node root2 = newNode(3);
+		root2.left = newNode(4);
+		root2.right = newNode(5);
+		root2.left.left = newNode(1);
+		root2.left.right = newNode(3);
+		root2.right = newNode(5);
+		root2.right.right = newNode(1);
+		System.out.println(maxProfit(root2));
 
-    public Result(int incl, int excl) {
-      this.incl = incl;
-      this.excl = excl;
-    }
-  }
+	}
+
+	private static class Profit {
+		int incl;
+		int excl;
+
+		public Profit(int incl, int excl) {
+			this.incl = incl;
+			this.excl = excl;
+		}
+	}
 }
