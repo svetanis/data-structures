d69315020db77087e91e106eaba188ca8f9c57c3
svetanis <svet125@gmail.com>
2025-01-17 22:43:19 -0700

    duplicates k dist away

--- src/main/java/com/svetanis/datastructures/hashmap/DuplicatesKDistAway.java
+++ src/main/java/com/svetanis/datastructures/hashmap/DuplicatesKDistAway.java
@@ -1,10 +1,13 @@
-package com.svetanis.datastructures.array;
+package com.svetanis.datastructures.hashmap;
 
 import static com.google.common.collect.Maps.newHashMap;
 import static java.util.Arrays.asList;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+// 219. Contains Duplicate II
 
 // given an integer array and integer k
 // return true if there are two distinct
@@ -13,6 +16,18 @@ import java.util.Map;
 
 public final class DuplicatesKDistAway {
 	// Time Complexity: O(n)
+
+	public static boolean duplicates(int[] a, int k) {
+		Map<Integer, Integer> map = new HashMap<>();
+		for (int i = 0; i < a.length; i++) {
+			int prev = map.getOrDefault(a[i], -1000000);
+			if ((i - prev) <= k) {
+				return true;
+			}
+			map.put(a[i], i);
+		}
+		return false;
+	}
 
 	public static boolean duplicates(List<Integer> list, int k) {
 		Map<Integer, Integer> map = newHashMap();
@@ -30,5 +45,12 @@ public final class DuplicatesKDistAway {
 		System.out.println(duplicates(asList(1, 2, 3, 1), 3));// true
 		System.out.println(duplicates(asList(1, 0, 1, 1), 1));// true
 		System.out.println(duplicates(asList(1, 2, 3, 1, 2, 3), 2)); // false
+
+		int[] a1 = { 1, 2, 3, 1 };
+		int[] a2 = { 1, 0, 1, 1 };
+		int[] a3 = { 1, 2, 3, 1, 2, 3 };
+		System.out.println(duplicates(a1, 3));// true
+		System.out.println(duplicates(a2, 1));// true
+		System.out.println(duplicates(a3, 2)); // false
 	}
 }
