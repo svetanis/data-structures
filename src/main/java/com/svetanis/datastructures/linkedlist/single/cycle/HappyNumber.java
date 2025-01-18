4aa80f833b5bb64d4e28b3974c957a887cc4cab5
svetanis <svet125@gmail.com>
2025-01-17 22:34:31 -0700

    happy number

--- src/main/java/com/svetanis/datastructures/linkedlist/single/cycle/HappyNumber.java
+++ src/main/java/com/svetanis/datastructures/linkedlist/single/cycle/HappyNumber.java
@@ -1,15 +1,16 @@
 package com.svetanis.datastructures.linkedlist.single.cycle;
 
+// 202. Happy Number
+
 // A number is called happy if it leads to 1 after a sequence of steps 
 // where in each step number is replaced by sum of squares of its digit 
 // that is if we start with Happy Number and 
 // keep replacing it with digits square sum, we reach 1.
 
 public final class HappyNumber {
+	// Time Complexity: O(log n)
 
   public static boolean happyNum(int n) {
-	// Time Complexity: O(log n)
-	  
     int slow = squareSum(n);
     int fast = squareSum(slow);
     while (slow != fast) {
