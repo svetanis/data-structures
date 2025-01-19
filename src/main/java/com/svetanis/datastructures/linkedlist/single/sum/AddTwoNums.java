bafd008ee44f1c28c796711650d187fa80eb0ebe
svetanis <svet125@gmail.com>
2025-01-18 21:25:49 -0700

    add two numbers

--- src/main/java/com/svetanis/datastructures/linkedlist/single/sum/AddTwoNums.java
+++ src/main/java/com/svetanis/datastructures/linkedlist/single/sum/AddTwoNums.java
@@ -1,57 +1,68 @@
 package com.svetanis.datastructures.linkedlist.single.sum;
 
-import static com.svetanis.datastructures.linkedlist.single.Nodes.insertAtHead;
+import static com.svetanis.datastructures.linkedlist.single.Nodes.fromList;
 import static com.svetanis.datastructures.linkedlist.single.Nodes.print;
+import static java.util.Arrays.asList;
 
 import com.svetanis.datastructures.linkedlist.single.Node;
 
+// 2. Add Two Numbers
+
 public final class AddTwoNums {
+	// Time Complexity: O(n + m)
+	// Space Complexity: O(1)
 
-  public static Node addTwoNumbers(Node first, Node second) {
-    
-    int carry = 0;
-    Node dummyHead = new Node(0);
-    Node curr = dummyHead;
-    while (first != null || second != null) {
-      int x = first != null ? first.data : 0;
-      int y = second != null ? second.data : 0;
-      int sum = x + y + carry;
-      carry = sum / 10;
+	public static Node addTwoLists(Node first, Node second) {
+		int carry = 0;
+		Node dummy = new Node(0);
+		Node curr = dummy;
+		while (first != null || second != null || carry != 0) {
+			int num1 = first == null ? 0 : first.data;
+			int num2 = second == null ? 0 : second.data;
+			int sum = num1 + num2 + carry;
+			carry = sum / 10;
+			// create a new node with digit value of the sum
+			curr.next = new Node(sum % 10);
+			// move to the next node in the result list
+			curr = curr.next;
+			// move first and second pointers to next node
+			first = first == null ? null : first.next;
+			second = second == null ? null : second.next;
+		}
+		return dummy.next;
+	}
 
-      curr.next = new Node(sum % 10);
-      curr = curr.next;
+	public static Node addTwoNumbers(Node first, Node second) {
+		int carry = 0;
+		Node dummy = new Node(0);
+		Node curr = dummy;
+		while (first != null || second != null) {
+			int x = first != null ? first.data : 0;
+			int y = second != null ? second.data : 0;
+			int sum = x + y + carry;
+			carry = sum / 10;
+			curr.next = new Node(sum % 10);
+			curr = curr.next;
+			first = first == null ? null : first.next;
+			second = second == null ? null : second.next;
+		}
+		if (carry > 0) {
+			curr.next = new Node(carry);
+		}
+		return dummy.next;
+	}
 
-      if (first != null) {
-        first = first.next;
-      }
-      if (second != null) {
-        second = second.next;
-      }
-    }
+	public static void main(String[] args) {
+		Node num1 = fromList(asList(2, 4, 3));
+		Node num2 = fromList(asList(5, 6, 4));
+		print(addTwoLists(num1, num2)); // 7 0 8
 
-    if (carry > 0) {
-      curr.next = new Node(carry);
-    }
-    return dummyHead.next;
-  }
+		Node num3 = fromList(asList(0));
+		Node num4 = fromList(asList(0));
+		print(addTwoLists(num3, num4)); // 0
 
-  public static void main(String[] args) {
-    Node head1 = null;
-    head1 = insertAtHead(head1, 6);
-    head1 = insertAtHead(head1, 4);
-    head1 = insertAtHead(head1, 9);
-    head1 = insertAtHead(head1, 5);
-    head1 = insertAtHead(head1, 7);
-
-    Node head2 = null;
-    head2 = insertAtHead(head2, 4);
-    head2 = insertAtHead(head2, 8);
-
-    System.out.println("Original lists:");
-    print(head1);
-    print(head2);
-    System.out.println("sum:");
-    Node sum = addTwoNumbers(head1, head2);
-    print(sum);
-  }
+		Node num5 = fromList(asList(9, 9, 9, 9, 9, 9, 9));
+		Node num6 = fromList(asList(9, 9, 9, 9));
+		print(addTwoLists(num5, num6)); // 8 9 9 9 0 0 0 1
+	}
 }
