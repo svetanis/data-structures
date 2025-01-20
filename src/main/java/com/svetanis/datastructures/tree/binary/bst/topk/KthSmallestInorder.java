ef7312cd454fec7aba280c4568e6fa6225b56798
svetanis <svet125@gmail.com>
2025-01-20 16:42:41 -0700

    kth smallest in order traversal

--- src/main/java/com/svetanis/datastructures/tree/binary/bst/topk/KthSmallestInorder.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bst/topk/KthSmallestInorder.java
@@ -1,14 +1,13 @@
 package com.svetanis.datastructures.tree.binary.bst.topk;
 
-import static com.google.common.base.Optional.absent;
-import static com.google.common.base.Optional.of;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
 
 import java.util.concurrent.atomic.AtomicInteger;
 
-import com.google.common.base.Optional;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// 230. K-th Smallest BST
 
 // given a BST and and integer k,
 // find k'th smallest element in BST
@@ -21,51 +20,49 @@ import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 // becomes equal to k
 
 public final class KthSmallestInorder {
+	// Time Complexity: O(log n + k)
+	// Space Complexity: O(log n)
 
-	public static Optional<Integer> kthSmallest(Node root, int k) {
-		// Time complexity: O(h + k)
-
+	public static int kthSmallest(Node root, int k) {
 		// count of nodes visited
 		AtomicInteger count = new AtomicInteger();
-		int kSmallest = kthSmallest(root, k, count);
-		return kSmallest != -1 ? of(kSmallest) : absent();
+		return kthSmallest(root, k, count);
 	}
 
 	private static int kthSmallest(Node root, int k, AtomicInteger count) {
-
 		if (isNull(root) || count.get() >= k) {
 			return -1;
 		}
-
 		// follow inorder traversal
 		// recur for left subtree first
 		int left = kthSmallest(root.left, k, count);
-
 		if (left != -1) {
 			return left;
 		}
-
 		// increment count of visited nodes
 		int curr = count.incrementAndGet();
-
 		// if count becomes k now, then
 		// this is the k'th smallest node
 		if (curr == k) {
 			return root.data;
 		}
-
 		// recur for right subtree
 		return kthSmallest(root.right, k, count);
 	}
 
 	public static void main(String[] args) {
-		Node root = newNode(20);
-		root.left = newNode(8);
-		root.right = newNode(22);
-		root.left.left = newNode(4);
-		root.left.right = newNode(12);
-		root.left.right.left = newNode(10);
-		root.left.right.right = newNode(14);
-		System.out.println(kthSmallest(root, 3));
+		Node root = newNode(5);
+		root.left = newNode(3);
+		root.right = newNode(6);
+		root.left.left = newNode(2);
+		root.left.right = newNode(4);
+		root.left.left.left = newNode(1);
+		System.out.println(kthSmallest(root, 3)); // 3
+
+		Node root1 = newNode(3);
+		root1.left = newNode(1);
+		root1.right = newNode(4);
+		root1.left.right = newNode(2);
+		System.out.println(kthSmallest(root1, 1)); // 1
 	}
 }
