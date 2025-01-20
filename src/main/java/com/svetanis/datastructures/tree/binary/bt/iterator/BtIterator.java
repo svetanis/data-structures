9907a277fef45be0721d666cdeb41354afd239f3
svetanis <svet125@gmail.com>
2025-01-20 13:25:14 -0700

    bst iterator

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/iterator/BtIterator.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/iterator/BtIterator.java
@@ -8,11 +8,13 @@ import java.util.Deque;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
+// 173. Binary Search Tree Iterator
+
 // Implement an iterator over a binary search tree (BST). 
 // Your iterator will be initialized with the root node of a BST. 
 // Calling next() will return the next smallest number in the BST. 
-// Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, 
-// where h is the height of the tree.
+// Note: next() and hasNext() should run in average O(1) time 
+// and uses O(h) memory, where h is the height of the tree.
 
 public final class BtIterator {
 
@@ -57,5 +59,22 @@ public final class BtIterator {
 		while (iter.hasNext()) {
 			System.out.print(iter.next() + " ");
 		}
+
+		Node root2 = newNode(7);
+		root2.left = newNode(3);
+		root2.right = newNode(15);
+		root2.right.left = newNode(9);
+		root2.right.right = newNode(20);
+		BtIterator iter2 = new BtIterator(root2);
+		System.out.println(iter2.next()); // 3
+		System.out.println(iter2.next()); // 7
+		System.out.println(iter2.hasNext()); // true
+		System.out.println(iter2.next()); // 9
+		System.out.println(iter2.hasNext()); // true
+		System.out.println(iter2.next()); // 15
+		System.out.println(iter2.hasNext()); // true
+		System.out.println(iter2.next()); // 20
+		System.out.println(iter2.hasNext()); // false
+
 	}
 }
