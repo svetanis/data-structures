ec0cf39bbcf2a6f45bfe923cac72e66adbb8ee33
svetanis <svet125@gmail.com>
2025-01-20 15:55:52 -0700

    zigzag level order traversal

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/lot/ZigZagQueue.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/lot/ZigZagQueue.java
@@ -1,41 +1,39 @@
 package com.svetanis.datastructures.tree.binary.bt.traversal.lot;
 
-import static com.google.common.collect.Lists.newArrayList;
-import static com.google.common.collect.Lists.newLinkedList;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
-import static com.svetanis.java.base.collect.Lists.newList;
-import static com.svetanis.java.base.utils.Print.printLists;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
-import com.google.common.collect.ImmutableList;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
-// Given a binary tree, populate an array to represent its zigzag level order traversal. 
-// You should populate the values of all nodes of the first level from left to right, 
-// then right to left for the next level and keep alternating in the same manner for the following levels.
+// 103. Binary Tree Zigzag Level Order Traversal
+
+// Given a binary tree, populate an array 
+// to represent its zigzag level order traversal. 
+// You should populate the values of all nodes of 
+// the first level from left to right, 
+// then right to left for the next level and 
+// keep alternating in the same manner for 
+// the following levels.
 
 public final class ZigZagQueue {
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
 
-	public static ImmutableList<ImmutableList<Integer>> traverse(Node root) {
-		// Time Complexity: O(n)
-		// Space Complexity: O(n)
-
-		if (isNull(root)) {
-			return newList();
+	public static List<List<Integer>> traverse(Node root) {
+		if (root == null) {
+			return new ArrayList<>();
 		}
-
 		boolean leftToRight = true;
-		Queue<Node> queue = newLinkedList();
+		Queue<Node> queue = new LinkedList<>();
 		queue.offer(root);
-		List<ImmutableList<Integer>> lists = newArrayList();
-
+		List<List<Integer>> lists = new ArrayList<>();
 		while (!queue.isEmpty()) {
 			int size = queue.size();
-			List<Integer> list = newLinkedList();
+			List<Integer> list = new LinkedList<>();
 			for (int i = 0; i < size; i++) {
 				Node node = queue.poll();
 				if (leftToRight) {
@@ -43,28 +41,25 @@ public final class ZigZagQueue {
 				} else {
 					list.add(0, node.data);
 				}
-				if (isNotNull(node.left)) {
+				if (node.left != null) {
 					queue.offer(node.left);
 				}
-				if (isNotNull(node.right)) {
+				if (node.right != null) {
 					queue.offer(node.right);
 				}
 			}
-			lists.add(newList(list));
+			lists.add(list);
 			leftToRight = !leftToRight;
 		}
-		return newList(lists);
+		return lists;
 	}
 
 	public static void main(String[] args) {
-		Node root = newNode(12);
-		root.left = newNode(7);
-		root.right = newNode(1);
-		root.left.left = newNode(9);
-		root.right.left = newNode(10);
-		root.right.right = newNode(5);
-		root.right.left.left = newNode(20);
-		root.right.left.right = newNode(17);
-		printLists(traverse(root));
+		Node root = newNode(3);
+		root.left = newNode(9);
+		root.right = newNode(20);
+		root.right.left = newNode(15);
+		root.right.right = newNode(7);
+		System.out.println(traverse(root)); // [3], [20,9],[15,7]
 	}
 }
