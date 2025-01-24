afd8165adfb9e0c33614dffaf385019647ca3bcd
svetanis <svet125@gmail.com>
2025-01-23 18:05:57 -0700

    palindrome

--- src/main/java/com/svetanis/datastructures/linkedlist/single/palindrome/Palindrome.java
+++ src/main/java/com/svetanis/datastructures/linkedlist/single/palindrome/Palindrome.java
@@ -2,45 +2,66 @@ package com.svetanis.datastructures.linkedlist.single.palindrome;
 
 import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.datastructures.linkedlist.single.Nodes.fromList;
-import static com.svetanis.datastructures.linkedlist.single.reverse.ReverseIterative.reverse;
-import static com.svetanis.datastructures.linkedlist.single.search.MiddleNode.middle;
 
 import com.svetanis.datastructures.linkedlist.single.Node;
+
+// 234. Palindrome Linked List
 
 // given the head of a Singly LinkedList
 // check if the SLL is a palindrom or not
 
 public final class Palindrome {
-  // Time Complexity: O(n)
+	// Time Complexity: O(n)
 
-  public static boolean isPalindrome(Node head) {
-    if (head == null || head.next == null) {
-      return true;
-    }
+	public static boolean isPalindrome(Node head) {
+		if (head == null || head.next == null) {
+			return true;
+		}
+		Node mid = middle(head);
+		Node reversed = reverse(mid);
+		Node curr1 = head;
+		Node curr2 = reversed;
+		while (curr2 != null) {
+			if (curr1.data != curr2.data) {
+				return false;
+			}
+			curr1 = curr1.next;
+			curr2 = curr2.next;
+		}
+		return true;
+	}
 
-    Node mid = middle(head);
-    Node reversed = reverse(mid);
-    Node copy = reversed;
+	public static Node middle(Node head) {
+		if (head == null) {
+			return null;
+		}
+		Node slow = head;
+		Node fast = head;
+		while (fast != null && fast.next != null) {
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return slow;
+	}
 
-    while (head != null && reversed != null) {
-      if (head.data != reversed.data) {
-        break;
-      }
-      head = head.next;
-      reversed = reversed.next;
-    }
+	public static Node reverse(Node head) {
+		Node prev = null;
+		Node curr = head;
+		while (curr != null) {
+			Node next = curr.next;
+			curr.next = prev;
+			prev = curr;
+			curr = next;
+		}
+		return prev;
+	}
 
-    reverse(copy);
-    return head == null || reversed == null;
-  }
+	public static void main(String[] args) {
+		// 0->1->2->1->0
+		Node head = fromList(newArrayList(0, 1, 2, 1, 0));
+		System.out.println(isPalindrome(head));
 
-  public static void main(String[] args) {
-    // 0->1->2->1->0
-    Node head = fromList(newArrayList(0, 1, 2, 1, 0));
-    System.out.println(isPalindrome(head));
-    
-    Node head1 = fromList(newArrayList(0, 1, 3, 0));
-    System.out.println(isPalindrome(head1));
-    
-  }
+		Node head1 = fromList(newArrayList(0, 1, 3, 0));
+		System.out.println(isPalindrome(head1));
+	}
 }
