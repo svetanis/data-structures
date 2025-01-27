9d529a6c3667ce4a085c630eef5ca05da5b7acfb
svetanis <svet125@gmail.com>
2025-01-26 20:29:41 -0700

    bt traversals

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/InOrderStack.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/InOrderStack.java
@@ -1,48 +1,47 @@
 package com.svetanis.datastructures.tree.binary.bt.traversal;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
-import static com.svetanis.java.base.collect.Lists.newList;
 import static com.svetanis.java.base.utils.Print.print;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
 import java.util.List;
-import java.util.Stack;
 
-import com.google.common.collect.ImmutableList;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
+// 94. Binary Tree Inorder Traversal
+
 public final class InOrderStack {
+	// Time complexity: O(n)
+	// Space complexity: O(n)
 
-  public static ImmutableList<Node> inOrder(Node root) {
-    // time complexity: O(n)
-    // space complexity: O(n)
+	public static List<Integer> inOrder(Node root) {
+		List<Integer> list = new ArrayList<>();
+		Deque<Node> stack = push(new ArrayDeque<>(), root);
+		while (!stack.isEmpty()) {
+			Node node = stack.pop();
+			list.add(node.data);
+			stack = push(stack, node.right);
+		}
+		return list;
+	}
 
-    List<Node> list = newArrayList();
-    Stack<Node> stack = push(new Stack<>(), root);
-    while (!stack.isEmpty()) {
-      Node node = stack.pop();
-      list.add(node);
-      stack = push(stack, node.right);
-    }
-    return newList(list);
-  }
+	private static Deque<Node> push(Deque<Node> stack, Node node) {
+		while (node != null) {
+			stack.push(node);
+			node = node.left;
+		}
+		return stack;
+	}
 
-  private static Stack<Node> push(Stack<Node> stack, Node node) {
-    while (isNotNull(node)) {
-      stack.push(node);
-      node = node.left;
-    }
-    return stack;
-  }
-
-  public static void main(String[] args) {
-    Node root = newNode(1);
-    root.left = newNode(2);
-    root.right = newNode(3);
-    root.left.left = newNode(4);
-    root.left.right = newNode(5);
-    root.right.right = newNode(6);
-    print(inOrder(root));
-  }
+	public static void main(String[] args) {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.right = newNode(6);
+		print(inOrder(root));
+	}
 }
--- src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/PostOrderTwoStacks.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/traversal/PostOrderTwoStacks.java
@@ -1,16 +1,16 @@
 package com.svetanis.datastructures.tree.binary.bt.traversal;
 
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNotNull;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
-import static com.svetanis.java.base.collect.Lists.newList;
-import static com.svetanis.java.base.utils.Arrays.toList;
 import static com.svetanis.java.base.utils.Print.print;
 
-import java.util.Stack;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 
-import com.google.common.collect.ImmutableList;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// 145. Binary Tree Postorder Traversal
 
 // Similar to preorder. Two differences :
 //   1) Where pre-order prints, post-order puts the value into another stack to reverse the order
@@ -18,35 +18,42 @@ import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
 public final class PostOrderTwoStacks {
 
-  public static ImmutableList<Node> postOrder(Node root) {
-    if (isNull(root)) {
-      return newList();
-    }
+	public static List<Integer> postOrder(Node root) {
+		if (root == null) {
+			return new ArrayList<>();
+		}
+		Deque<Node> stack = new ArrayDeque<>();
+		Deque<Node> out = new ArrayDeque<>();
+		stack.push(root);
+		while (!stack.isEmpty()) {
+			Node node = stack.pop();
+			out.push(node);
 
-    Stack<Node> stack = new Stack<>();
-    Stack<Node> out = new Stack<>();
-    stack.push(root);
-    while (!stack.isEmpty()) {
-      Node node = stack.pop();
-      out.push(node);
+			if (node.left != null) {
+				stack.push(node.left);
+			}
+			if (node.right != null) {
+				stack.push(node.right);
+			}
+		}
+		return toList(out);
+	}
 
-      if (isNotNull(node.left)) {
-        stack.push(node.left);
-      }
-      if (isNotNull(node.right)) {
-        stack.push(node.right);
-      }
-    }
-    return toList(out);
-  }
+	public static List<Integer> toList(Deque<Node> queue) {
+		List<Integer> list = new ArrayList<>();
+		while (!queue.isEmpty()) {
+			list.add(queue.poll().data);
+		}
+		return list;
+	}
 
-  public static void main(String[] args) {
-    Node root = newNode(1);
-    root.left = newNode(2);
-    root.right = newNode(3);
-    root.left.left = newNode(4);
-    root.left.right = newNode(5);
-    root.right.right = newNode(6);
-    print(postOrder(root));
-  }
+	public static void main(String[] args) {
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		root.right.right = newNode(6);
+		print(postOrder(root));
+	}
 }
