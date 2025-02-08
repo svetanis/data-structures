a5ddcc80cbe99cae8b76ee9f20d316fc25665ca1
svetanis <svet125@gmail.com>
2025-02-08 14:08:40 -0700

    convert bst to sorted dll

---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/convert/ConvertBstToSortedDll.java
@@ -0,0 +1,47 @@
+package com.svetanis.datastructures.tree.binary.bt.convert;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+public final class ConvertBstToSortedDll {
+	// Time complexity: O(n)
+
+	private Node prev;
+	private Node head;
+
+	public Node bstToDll(Node root) {
+		if (root == null) {
+			return null;
+		}
+		prev = null;
+		head = null;
+		inOrder(root);
+		prev.right = head;
+		head.left = prev;
+		return head;
+	}
+
+	private void inOrder(Node root) {
+		if (root == null) {
+			return;
+		}
+		inOrder(root.left);
+		if (prev != null) {
+			prev.right = root;
+			root.left = prev;
+		} else {
+			head = root;
+		}
+		prev = root;
+		inOrder(root.right);
+	}
+
+	public static void main(String[] args) {
+		Node root = new Node(4);
+		root.left = new Node(2);
+		root.right = new Node(5);
+		root.left.left = new Node(1);
+		root.left.right = new Node(3);
+		ConvertBstToSortedDll bts = new ConvertBstToSortedDll();
+		bts.bstToDll(root);
+	}
+}
