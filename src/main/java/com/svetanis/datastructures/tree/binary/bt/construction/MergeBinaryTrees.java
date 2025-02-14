181f501e769444936bbec150b60fc60be88889e7
svetanis <svet125@gmail.com>
2025-02-14 13:35:15 -0700

    merge binary trees

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/construction/MergeBinaryTrees.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/construction/MergeBinaryTrees.java
@@ -2,27 +2,31 @@ package com.svetanis.datastructures.tree.binary.bt.construction;
 
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.inOrder;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
 
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
-// Given two binary trees and imagine that when you put one of them to cover the other, 
-// some nodes of the two trees are overlapped while the others are not.
+// 617. Merge Two Binary Trees
 
-// You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, 
+// Given two binary trees and imagine that 
+// when you put one of them to cover the other, 
+// some nodes of the two trees are overlapped 
+// while the others are not.
+
+// You need to merge them into a new binary tree. 
+// The merge rule is that if two nodes overlap, 
 // then sum node values up as the new value of the merged node. 
 // Otherwise, the NOT null node will be used as the node of new tree.
 
 public final class MergeBinaryTrees {
 
   public static Node merge(Node root1, Node root2) {
-    if (isNull(root1)) {
+    if (root1 == null) {
       return root2;
     }
-    if (isNull(root2)) {
+    if (root2 == null) {
       return root1;
     }
-    Node root = newNode(root1.data + root2.data);
+    Node root = new Node(root1.data + root2.data);
     root.left = merge(root1.left, root2.left);
     root.right = merge(root1.right, root2.right);
     return root;
