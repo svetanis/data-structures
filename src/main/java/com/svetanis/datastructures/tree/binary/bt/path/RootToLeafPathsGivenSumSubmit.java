aa0d721f07b31d399c8219d540110f11ae4ccf45
svetanis <svet125@gmail.com>
2025-02-22 19:45:04 -0700

    root to leaf path given sum

--- src/main/java/com/svetanis/datastructures/tree/binary/bt/path/RootToLeafPathsGivenSumSubmit.java
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/path/RootToLeafPathsGivenSumSubmit.java
@@ -1,18 +1,12 @@
 package com.svetanis.datastructures.tree.binary.bt.path;
 
-import static com.google.common.collect.Lists.newArrayList;
 import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.inOrder;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isLeaf;
-import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Nodes.isNull;
-import static com.svetanis.java.base.collect.Lists.newList;
-import static com.svetanis.java.base.utils.Print.printLists;
 
 import java.util.ArrayDeque;
+import java.util.ArrayList;
 import java.util.Deque;
 import java.util.List;
 
-import com.google.common.collect.ImmutableList;
 import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 
 // 113. Path Sum II
@@ -21,18 +15,18 @@ import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
 // find all paths from root-to-leaf such that
 // the sum of all the node values of each path equals ‘S’.
 
-public final class RootToLeafPathsGivenSum {
+public final class RootToLeafPathsGivenSumSubmit {
 	// Time complexity: O(n)
 
-	public static ImmutableList<ImmutableList<Integer>> paths(Node node, int sum) {
+	public static List<List<Integer>> paths(Node node, int target) {
 		Deque<Integer> path = new ArrayDeque<>();
-		List<ImmutableList<Integer>> paths = newArrayList();
-		paths(node, sum, path, paths);
-		return newList(paths);
+		List<List<Integer>> paths = new ArrayList<>();
+		paths(node, target, path, paths);
+		return paths;
 	}
 
-	private static void paths(Node node, int sum, Deque<Integer> path, List<ImmutableList<Integer>> paths) {
-		if (isNull(node)) {
+	private static void paths(Node node, int target, Deque<Integer> path, List<List<Integer>> paths) {
+		if (node == null) {
 			return;
 		}
 		// add the current node to the path
@@ -40,13 +34,13 @@ public final class RootToLeafPathsGivenSum {
 		// if the current node is a leaf &
 		// its value is equal to sum,
 		// save the current path
-		if (isLeaf(node) && sum == node.data) {
-			paths.add(newList(path));
+		if (node.left == null && node.right == null && target == node.data) {
+			paths.add(new ArrayList<>(path));
 		} else {
 			// traverse left subtree
-			paths(node.left, sum - node.data, path, paths);
+			paths(node.left, target - node.data, path, paths);
 			// traverse right subtree
-			paths(node.right, sum - node.data, path, paths);
+			paths(node.right, target - node.data, path, paths);
 		}
 		// backtrack: remove the current node from the path
 		// while going up the recursive call stack
@@ -64,11 +58,6 @@ public final class RootToLeafPathsGivenSum {
 		root.left.left = newNode(3);
 		root.left.right = newNode(5);
 		root.right.left = newNode(2);
-
-		inOrder(root);
-		System.out.println();
-
-		// given sum root-to-leaf path
-		printLists(paths(root, 21));
+		System.out.println(paths(root, 21));
 	}
 }
