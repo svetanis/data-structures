68832ea8864a6d2fdb6476fef7782246bc003544
svetanis <svet125@gmail.com>
2025-03-30 14:21:16 -0700

    number of operations to make network connected

---
+++ src/main/java/com/svetanis/datastructures/graph/unionfind/OperationsToMakeNetworkConnected.java
@@ -0,0 +1,53 @@
+package com.svetanis.datastructures.graph.unionfind;
+
+// 1319. Number of Operations to Make Network Connected
+
+public final class OperationsToMakeNetworkConnected {
+	// Time Complexity: O(connections * log n)
+
+	private int[] parent;
+
+	public int network(int n, int[][] connections) {
+		init(n);
+		int redundant = 0;
+		for (int[] connection : connections) {
+			int x = connection[0];
+			int y = connection[1];
+			int px = find(x);
+			int py = find(y);
+			if (px != py) {
+				parent[px] = py;
+				n -= 1;
+			} else {
+				redundant++;
+			}
+		}
+		return n - 1 > redundant ? -1 : n - 1;
+	}
+
+	private void init(int n) {
+		this.parent = new int[n];
+		for (int i = 0; i < n; i++) {
+			this.parent[i] = i;
+		}
+	}
+
+	private int find(int x) {
+		if (parent[x] != x) {
+			parent[x] = find(parent[x]);
+		}
+		return parent[x];
+	}
+
+	public static void main(String[] args) {
+		OperationsToMakeNetworkConnected mnc = new OperationsToMakeNetworkConnected();
+		int[][] g1 = { { 0, 1 }, { 0, 2 }, { 1, 2 } };
+		System.out.println(mnc.network(4, g1)); // 1
+
+		int[][] g2 = { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 }, { 1, 3 } };
+		System.out.println(mnc.network(6, g2)); // 2
+
+		int[][] g3 = { { 0, 1 }, { 0, 2 }, { 0, 3 }, { 1, 2 } };
+		System.out.println(mnc.network(6, g3)); // -1
+	}
+}
