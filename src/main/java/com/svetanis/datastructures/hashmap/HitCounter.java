7cddd38262ed5be0ab23a22916c29064b8d40776
svetanis <svet125@gmail.com>
2025-03-29 19:52:43 -0700

    hit counter

---
+++ src/main/java/com/svetanis/datastructures/hashmap/HitCounter.java
@@ -0,0 +1,43 @@
+package com.svetanis.datastructures.hashmap;
+
+import java.util.HashMap;
+import java.util.Map;
+
+// 362. Design Hit Counter
+
+// design a system that keeps track 
+// of the number of hits (or actions/events) 
+// that occur on a resource in the last 5 minutes.
+
+public final class HitCounter {
+	// Time Complexity: O(n)
+	// Space Complexity: O(p)
+
+	private Map<Integer, Integer> map;
+
+	public HitCounter() {
+		this.map = new HashMap<>();
+	}
+
+	public void hit(int timestamp) {
+		map.put(timestamp, map.getOrDefault(timestamp, 0) + 1);
+	}
+
+	public int getHits(int timestamp) {
+		int hits = 0;
+		for (int key : map.keySet()) {
+			if (timestamp - key < 300) {
+				hits += map.get(key);
+			}
+		}
+		return hits;
+	}
+
+	public static void main(String[] args) {
+		HitCounter hc = new HitCounter();
+		hc.hit(10);
+		hc.hit(10);
+		hc.hit(30);
+		System.out.println(hc.getHits(40)); // 3
+	}
+}
