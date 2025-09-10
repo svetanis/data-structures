4b4f48a157f70d26c94fa10b9d284240991f07ab
svetanis <svet125@gmail.com>
2025-09-09 17:07:03 -0700

    bt dimension

---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/dimention/DiameterOptimized.java
@@ -0,0 +1,45 @@
+package com.svetanis.datastructures.tree.binary.bt.dimention;
+
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// 543. Diameter of Binary Tree
+
+// Given a binary tree, find the length of its diameter. 
+// The diameter of a tree is the number of edges on 
+// the longest path between any two leaf nodes. 
+// The diameter of a tree may or may not pass through the root.
+
+public final class DiameterOptimized {
+	// Time complexity: O(n)
+
+	private int maxDiameter;
+
+	public int diameter(Node root) {
+		this.maxDiameter = 0;
+		dfs(root);
+		return maxDiameter;
+	}
+
+	private int dfs(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = dfs(root.left);
+		int right = dfs(root.right);
+		int diameterThroughNode = left + right;
+		maxDiameter = Math.max(maxDiameter, diameterThroughNode);
+		return 1 + Math.max(left, right);
+	}
+
+	public static void main(String[] args) {
+		DiameterOptimized dop = new DiameterOptimized();
+		Node root = newNode(1);
+		root.left = newNode(2);
+		root.right = newNode(3);
+		root.left.left = newNode(4);
+		root.left.right = newNode(5);
+		System.out.println(dop.diameter(root)); // 3
+	}
+}
---
+++ src/main/java/com/svetanis/datastructures/tree/binary/bt/dimention/LongestUnivaluePath.java
@@ -0,0 +1,53 @@
+package com.svetanis.datastructures.tree.binary.bt.dimention;
+
+import static com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node.newNode;
+
+import com.svetanis.datastructures.tree.binary.model.mutable.primitive.Node;
+
+// 687. Longest Univalue Path
+
+public final class LongestUnivaluePath {
+	// Time complexity: O(n)
+
+	private int maxLen;
+
+	public int longestUnivaluePath(Node root) {
+		this.maxLen = 0;
+		dfs(root);
+		return maxLen;
+	}
+
+	private int dfs(Node root) {
+		if (root == null) {
+			return 0;
+		}
+		int left = dfs(root.left);
+		int right = dfs(root.right);
+		boolean lup = root.left != null && root.left.data == root.data;
+		left = lup ? left + 1 : 0;
+		boolean rup = root.right != null && root.right.data == root.data;
+		right = rup ? right + 1 : 0;
+		int pathThroughNode = left + right;
+		maxLen = Math.max(maxLen, pathThroughNode);
+		return Math.max(left, right);
+	}
+
+	public static void main(String[] args) {
+		LongestUnivaluePath dop = new LongestUnivaluePath();
+		Node root = newNode(5);
+		root.left = newNode(4);
+		root.right = newNode(5);
+		root.left.left = newNode(1);
+		root.left.right = newNode(1);
+		root.right.right = newNode(5);
+		System.out.println(dop.longestUnivaluePath(root)); // 2
+
+		Node root2 = newNode(1);
+		root2.left = newNode(4);
+		root2.right = newNode(5);
+		root2.left.left = newNode(4);
+		root2.left.right = newNode(4);
+		root2.right.right = newNode(5);
+		System.out.println(dop.longestUnivaluePath(root2)); // 2
+	}
+}
