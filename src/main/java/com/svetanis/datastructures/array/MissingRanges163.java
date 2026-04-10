46af1295771e4d16a7da5aa4e69123ee292d6063
svetanis <svet125@gmail.com>
2026-04-09 19:31:50 -0700

    missing ranges

--- src/main/java/com/svetanis/datastructures/array/MissingRanges163.java
+++ src/main/java/com/svetanis/datastructures/array/MissingRanges163.java
@@ -7,34 +7,49 @@ import java.util.List;
 // 163. Missing Ranges
 
 public final class MissingRanges163 {
-  // Time Complexity: O(n)
-  // Space Complexity: O(n)
+	// Time Complexity: O(n)
+	// Space Complexity: O(n)
 
-  public static List<List<Integer>> missingRanges(int[] a, int lower, int upper) {
-    int n = a.length;
-    List<List<Integer>> list = new ArrayList<>();
+	public static List<List<Integer>> missingRangesUnified(int[] a,
+			int lower, int upper) {
+		int n = a.length;
+		int prev = lower - 1;
+		List<List<Integer>> list = new ArrayList<>();
+		for (int i = 0; i <= n; i++) {
+			int curr = (i < n) ? a[i] : upper + 1;
+			if (curr - prev > 1) {
+				list.add(Arrays.asList(prev + 1, curr - 1));
+			}
+			prev = curr;
+		}
+		return list;
+	}
 
-    if (n == 0) {
-      list.add(Arrays.asList(lower, upper));
-      return list;
-    }
-    if (a[0] > lower) {
-      list.add(Arrays.asList(lower, a[0] - 1));
-    }
-    for (int i = 1; i < n; i++) {
-      if (a[i] - a[i - 1] > 1) {
-        list.add(Arrays.asList(a[i - 1] + 1, a[i] - 1));
-      }
-    }
-    if (a[n - 1] < upper) {
-      list.add(Arrays.asList(a[n - 1] + 1, upper));
-    }
-    return list;
-  }
+	public static List<List<Integer>> missingRanges(int[] a, int lower, int upper) {
+		int n = a.length;
+		List<List<Integer>> list = new ArrayList<>();
 
-  public static void main(String[] args) {
-    int[] a = { 2, 3, 7, 9 };
-    // [[1, 1], [4, 6], [8, 8], [10, 10]]
-    System.out.println(missingRanges(a, 1, 10));
-  }
+		if (n == 0) {
+			list.add(Arrays.asList(lower, upper));
+			return list;
+		}
+		if (a[0] > lower) {
+			list.add(Arrays.asList(lower, a[0] - 1));
+		}
+		for (int i = 1; i < n; i++) {
+			if (a[i] - a[i - 1] > 1) {
+				list.add(Arrays.asList(a[i - 1] + 1, a[i] - 1));
+			}
+		}
+		if (a[n - 1] < upper) {
+			list.add(Arrays.asList(a[n - 1] + 1, upper));
+		}
+		return list;
+	}
+
+	public static void main(String[] args) {
+		int[] a = { 2, 3, 7, 9 };
+		// [[1, 1], [4, 6], [8, 8], [10, 10]]
+		System.out.println(missingRanges(a, 1, 10));
+	}
 }
